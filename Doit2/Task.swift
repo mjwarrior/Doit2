dd9db2573a6b7a726822065c9d00cc30bf5fabdb
marcstreeter <mstreeter@gmail.com>
2016-10-25 02:01:42 -0500

    Lecture 58/6:14  getting cell text from array of Tasks

---
+++ Doit2/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit2
+//
+//  Created by Marc Streeter on 10/25/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
