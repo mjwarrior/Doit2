5ba970d800866d03c521f835bd9dfb858ac6617b
marcstreeter <mstreeter@gmail.com>
2016-10-25 02:09:06 -0500

    Lecture 58/7:18  added exclamation mark logic

--- Doit2/ViewController.swift
+++ Doit2/ViewController.swift
@@ -31,7 +31,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let task = tasks[indexPath.row]
-        cell.textLabel?.text = task.name
+        
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
         return cell
     }
     
